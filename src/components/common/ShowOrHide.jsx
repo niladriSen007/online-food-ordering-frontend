1dcbd01564ca9c88f13fb868d17506f945b0dbb7
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2024-10-15 19:08:18 +0530

    Update ShowOrHide.jsx tsx

--- src/components/common/ShowOrHide.jsx
+++ src/components/common/ShowOrHide.jsx
@@ -3,3 +3,23 @@ const ShowOrHide = ({ when, fallback = null, child }) => {
   return when ? child : fallback
 }
 export default ShowOrHide
+
+
+
+
+//In ts
+
+import React from 'react';
+
+interface ShowOrHideProps {
+  when: boolean;
+  fallback?: React.ReactNode;
+  child: React.ReactNode;
+}
+
+const ShowOrHide: React.FC<ShowOrHideProps> = ({ when, fallback = null, child }) => {
+  console.log(when);
+  return when ? child : fallback;
+}
+
+export default ShowOrHide;
